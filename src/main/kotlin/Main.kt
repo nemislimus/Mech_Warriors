73c1d22a1cd8e65bb615a096d7427db29dece5e3
nemislimus <151661403+nemislimus@users.noreply.github.com>
2024-04-25 11:38:26 +0300

    Revert "Start"

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -15,12 +15,12 @@ class Gun(private val model: String, private val ammo: Int, private val damage: 
         return 0 //При перезарядке выстрел пропускается
     }
 
-    private fun reloadAmmo() {
-        ammoCount = ammo
-    }
-
     fun getAmmoInfo() {
         println("Пушка $model. Осталось зарядов: $ammoCount")
+    }
+
+    private fun reloadAmmo() {
+        ammoCount = ammo
     }
 
     init {
@@ -28,14 +28,14 @@ class Gun(private val model: String, private val ammo: Int, private val damage: 
     }
 }
 
-class MechWarrior(private val name: String, private var healthPoint: Int, val firstGun: Gun, val secondGun: Gun) {
+class MechWarrior(private val name: String, private var healthPoint: Int, val rightGun: Gun, val leftGun: Gun) {
 
     var isAlive: Boolean = true
 
-    fun attackEnemy(enemy: MechWarrior) {
-        println("!!!!!$name атакует ${enemy.name}!!!!!")
-        enemy.takeDamage(firstGun.fire())
-        enemy.takeDamage(secondGun.fire())
+    fun attackMech(mech: MechWarrior) {
+        println("!!!!!$name атакует ${mech.name}!!!!!")
+        mech.takeDamage(rightGun.fire())
+        mech.takeDamage(leftGun.fire())
         println()
     }
 
@@ -73,13 +73,13 @@ object Duel {
                 for (defender in mechs) {
 
                     if (defender != striker) {
-                        striker.attackEnemy(defender)
+                        striker.attackMech(defender)
                         if (!stillAlive(defender)) return defender
                     } else continue
                 }
             }
             round++
-            Thread.sleep(1000)
+            Thread.sleep(1500)
         }
     }
 
@@ -113,37 +113,6 @@ fun main() {
     println()
 
     println("Лут с погибшего ${deadOne.getName()}:")
-    deadOne.secondGun.getAmmoInfo()
-    deadOne.firstGun.getAmmoInfo()
+    deadOne.leftGun.getAmmoInfo()
+    deadOne.rightGun.getAmmoInfo()
 }
-
-
-
-//const val ANSI_RESET = "\u001B[0m"
-//const val ANSI_YELLOW = "\u001B[33m"
-//const val ANSI_RED = "\u001B[31m"
-//const val ANSI_GREEN = "\u001B[32m"
-
-//// ANSI Escape коды для установки цвета текста
-//val red = "\u001B[31m"
-//val green = "\u001B[32m"
-//val blue = "\u001B[34m"
-//val reset = "\u001B[0m" // Сброс цвета обратно к стандартному
-//
-//// Примеры текста с разными цветами
-//println("${red}Красный текст${reset}")
-//println("${green}Зеленый текст${reset}")
-//println("${blue}Синий текст${reset}")
-
-//Черный (\u001B[30m)
-//Красный (\u001B[31m)
-//Зеленый (\u001B[32m)
-//Желтый (\u001B[33m)
-//Синий (\u001B[34m)
-//Маджента (пурпурный) (\u001B[35m)
-//Циан (голубой) (\u001B[36m)
-//Белый (\u001B[37m)
-
-
-
-//create commit for reset testing. START_SPOT
